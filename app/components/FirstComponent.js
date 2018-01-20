967fba9a49ef63fd6d8b593e640669aefb7d20e3
Jose <canedo.j.j@gmail.com>
2018-01-20 10:24:15 -0500

    Setup React Project

---
+++ app/components/FirstComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class FirstComponent extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+
+}
+
+export default FirstComponent;
